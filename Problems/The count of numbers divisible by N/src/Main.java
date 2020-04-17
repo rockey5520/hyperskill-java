13dc50bd1eb34a2d94d29f4a06e4755e30bbcae0
Rakesh Mothukuri <rockey5520@gmail.com>
2020-04-17 20:59:25 +0100

    count of numbers

---
+++ Problems/The count of numbers divisible by N/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int min = scanner.nextInt();
+        int max = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        int counter = 0;
+        for (int i = min; i <= max ; i++) {
+            if(i%n == 0){
+                counter++;
+            }
+        }
+        System.out.println(counter);
+
+    }
+}
