c44e6bcacd45e374d7b4613167b6a3abce1c09a4
Rakesh Mothukuri <rockey5520@gmail.com>
2020-04-17 22:04:53 +0100

    numbers divisible by 6

---
+++ Problems/Numbers divisible by six/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.Scanner;
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sum = 0;
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < n ; i++) {
+            list.add(scanner.nextInt());
+        }
+        for (int i: list
+        ) {
+            if(i%6== 0 ){
+                sum = sum + i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
