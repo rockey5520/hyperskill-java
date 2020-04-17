020ed45ab538d4c61537a722215e1d304aa99f36
Rakesh Mothukuri <rockey5520@gmail.com>
2020-04-17 19:36:14 +0100

    healthy sleep solution

---
+++ Problems/Healthy sleep/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int min_hours = scanner.nextInt();
+        int max_hours = scanner.nextInt();
+        int slept_hours = scanner.nextInt();
+
+        if(slept_hours >= min_hours && slept_hours <= max_hours){
+            System.out.println("Normal");
+        }else if( slept_hours < min_hours){
+            System.out.println("Deficiency");
+        }else if( slept_hours > max_hours){
+            System.out.println("Excess");
+        }
+
+
+    }
+}
