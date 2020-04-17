a2ca9035f7738f30597d7f04f6be458a40253fac
Rakesh Mothukuri <rockey5520@gmail.com>
2020-04-17 19:42:35 +0100

    Triangle solution

---
+++ Problems/Triangle/src/Main.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if(((a+b) > c) && ((b+c) > a) && ((c+a) > b) ){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
